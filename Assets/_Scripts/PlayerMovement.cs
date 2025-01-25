288f00f3421ae50c628057aa3518309fe718c794
SwPGameDev <swpgamedev@gmail.com>
2025-01-25 12:28:56 -0700

    Very basic on foot movement

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -1,16 +1,37 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Refernces")]
+    [SerializeField] Camera cam;
+    [SerializeField] Rigidbody rb;
+
+    // MOVEMENT
+    InputAction movementInput;
+    Vector2 movementVector;
+
+    [Header("Movement Stats")]
+    [SerializeField] float moveSpeed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        movementInput = InputSystem.actions.FindAction("Move");
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movementVector = movementInput.ReadValue<Vector2>() * moveSpeed;
+
+        print("Movement Vector: " +  movementVector);
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector3(movementVector.x, rb.linearVelocity.y, movementVector.y);
     }
 }
