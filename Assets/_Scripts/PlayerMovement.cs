6ef6371440c16394ff331a6a7abc874e5e98f68a
Chris <32988106+Noobot9k@users.noreply.github.com>
2025-01-28 01:50:41 -0700

    Improve on-foot character feel

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -2,18 +2,13 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
-{
-    [Header("Refernces")]
+public class PlayerMovement : MonoBehaviour {
+    //[Header("Refernces")]
     Camera cam;
     Rigidbody rb;
 
     // MOVEMENT
     [SerializeField] float playerHeight = 2;
-
-    //InputAction lateralInput;
-    //InputAction dashInput;
-    //InputAction jumpInput;
 
     public bool isDashPressed = false;
     public bool isJumpPressed = false;
@@ -22,6 +17,7 @@ public class PlayerMovement : MonoBehaviour
 
     bool lastDashPressed = false;
     bool lastJumpPressed = false;
+    Vector3 gravity;
 
     [Header("Movement Stats")]
     [SerializeField] float acceleration;
@@ -29,6 +25,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float jumpForce;
     [SerializeField] float dashForce;
     [SerializeField] float airStrafe;
+    [Tooltip("Slerp rotate the player's velocity towards their desired movement direction to turn faster than acceleration allows.")]
+    [SerializeField] float velocitySlerp = 5;
+    [Tooltip("Gravity will be multiplied by this value for this character after reaching the apex of their jump or releasing the jump key.")]
+    [SerializeField] float fallingGravityModifier = 2;
 
     [Header("Jump")]
     [SerializeField] bool canJump;
@@ -51,43 +51,36 @@ public class PlayerMovement : MonoBehaviour
     Vector3 camForward;
     Vector3 camRight;
 
-    void Start()
-    {
+    void Start() {
+        gravity = Physics.gravity;
         cam = Camera.main;
-        //lateralInput = InputSystem.actions.FindAction("Move");
-        //dashInput = InputSystem.actions.FindAction("Sprint");
-        //jumpInput = InputSystem.actions.FindAction("Jump");
         rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
-    {
+    void Update() {
+        // Uh, bryce? This is all physics code. It should probably be in fixed update.
+
         bool isJumpThisFrame = isJumpPressed && isJumpPressed != lastJumpPressed;
         bool isDashThisFrame = isDashPressed && isDashPressed != lastDashPressed;
         lastJumpPressed = isJumpPressed;
         lastDashPressed = isDashPressed;
 
 
-        if (!canJump && grounded)
-        {
+        if(!canJump && grounded) {
             jumpTimer += Time.deltaTime;
-            if (jumpTimer >= jumpCooldown)
-            {
+            if(jumpTimer >= jumpCooldown) {
                 jumpTimer = 0;
                 canJump = true;
             }
         }
 
-        if (!canDash && grounded)
-        {
+        if(!canDash && grounded) {
             dashTimer += Time.deltaTime;
-            if (dashTimer >= jumpCooldown)
-            {
+            if(dashTimer >= jumpCooldown) {
                 dashTimer = 0;
                 canDash = true;
             }
         }
-
 
 
 
@@ -105,51 +98,72 @@ public class PlayerMovement : MonoBehaviour
         relativeMovementVector = (forwardRelative + rightRelative).normalized * moveSpeed;
         relativeMovementVector.y = 0f;
 
-        if (grounded)
-        {
+        float rateOfVelocityChange;
 
-            rb.linearVelocity = Vector3.MoveTowards(rb.linearVelocity, new Vector3(relativeMovementVector.x, rb.linearVelocity.y, relativeMovementVector.z), acceleration * Time.deltaTime);
+        if(grounded) {
 
-            if (isJumpThisFrame) // jumpInput.WasPressedThisFrame())
-            {
+            rateOfVelocityChange = acceleration;
+
+            if(relativeMovementVector.sqrMagnitude > Mathf.Epsilon) {
+                if(Vector3.Dot(rb.linearVelocity.normalized, relativeMovementVector) > 0) { //accelerating
+
+                    /* rotate the player's velocity towards their desired movement direction faster
+                    than acceleration would normally allow so they don't feel like they're on ice. */
+                    float blend = 1 - Mathf.Pow(0.5f, Time.deltaTime * velocitySlerp);
+                    rb.linearVelocity = Vector3.RotateTowards(rb.linearVelocity, relativeMovementVector.normalized * rb.linearVelocity.magnitude + Vector3.up * rb.linearVelocity.y, blend, 0);
+
+                } else { //decelerating
+
+                    // abruptly stop when trying to run the opposite way.
+                    rb.linearVelocity = Vector3.ProjectOnPlane(rb.linearVelocity, relativeMovementVector.normalized);
+
+                }
+            }
+
+            if(isJumpThisFrame) { // jumpInput.WasPressedThisFrame())
                 canJump = false;
                 rb.AddRelativeForce(new Vector3(relativeMovementVector.x, jumpForce, relativeMovementVector.z), ForceMode.Impulse);
                 //rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y + jumpForce, rb.linearVelocity.z);
-            }
-            else if (isDashThisFrame) //dashInput.WasPressedThisFrame())
-            {
+            } else if(isDashThisFrame) { //dashInput.WasPressedThisFrame())
                 canDash = false;
                 rb.AddRelativeForce(new Vector3(relativeMovementVector.x, 0, relativeMovementVector.z) * dashForce, ForceMode.Impulse);
             }
-        }
-        else
-        {
-            if (movementVector.sqrMagnitude > Mathf.Epsilon)
-            {
+            rb.linearVelocity += Time.deltaTime * gravity;
+
+        } else { // airborne
+
+            /*if(movementVector.sqrMagnitude > Mathf.Epsilon) {
                 rb.linearVelocity += (new Vector3(relativeMovementVector.x, 0, relativeMovementVector.z) * airStrafe) * Time.deltaTime;
                 //rb.AddRelativeForce(new Vector3(relativeMovementVector.x, 0, relativeMovementVector.z) * airStrafe, ForceMode.Force);
-            }
-            
+            }*/
+            rateOfVelocityChange = airStrafe;
+
+            /* Increase gravity after the climax of our jump or releasing the jump key to make the
+            character feel less floaty and allow the player to vary their jump height. */
+            if (rb.linearVelocity.y > 0 && isJumpPressed)
+                rb.linearVelocity += Time.deltaTime * gravity;
+            else
+                /* Apparently, putting floats to the left of vectors when multiplying is better for
+                performance. I'm sure the compiler just does that for us though. */
+                rb.linearVelocity += fallingGravityModifier * Time.deltaTime * gravity;
         }
 
-
+        // acceleration
+        rb.linearVelocity = Vector3.MoveTowards(rb.linearVelocity, relativeMovementVector + Vector3.up * rb.linearVelocity.y, rateOfVelocityChange * Time.deltaTime);
     }
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
 
         GroundCheck();
-        
+
     }
 
-    void GroundCheck()
-    {
+    void GroundCheck() {
         grounded = Physics.CheckSphere(new Vector3(transform.position.x, transform.position.y - (playerHeight / 2), transform.position.z), groundCheckRadius, groundLayer);
         //grounded = Physics.Raycast(transform.position, Vector3.down, (playerHeight / 2) + 0.1f);
     }
 
-    private void OnDrawGizmos()
-    {
+    private void OnDrawGizmos() {
         Vector3 playerPos = transform.position;
         playerPos.y = 0f;
         Gizmos.DrawLine(playerPos, playerPos + relativeMovementVector);
