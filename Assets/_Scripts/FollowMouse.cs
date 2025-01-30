ab596d65030b3928766a4cf53d27a19a8b677b5e
Chris <32988106+Noobot9k@users.noreply.github.com>
2025-01-29 19:05:17 -0700

    limit telekinesis range

--- Assets/_Scripts/FollowMouse.cs
+++ Assets/_Scripts/FollowMouse.cs
@@ -1,8 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class FollowMouse : MonoBehaviour
-{
+public class FollowMouse : MonoBehaviour {
     public Vector3 pointerPos;
     Camera cam;
     [SerializeField] GameObject player;
@@ -11,64 +10,34 @@ public class FollowMouse : MonoBehaviour
     [SerializeField] float flySpeed;
     [SerializeField] LayerMask layerMask;
     [SerializeField] float yOffset = 0.5f;
+    [SerializeField] float maxRange = 30;
     public bool matchPlayerHeight = true;
-    bool holding = false;
-    Vector3 holdingScreenPos;
 
-    void Start()
-    {
+    void Start() {
         cam = Camera.main;
         cameraAction = InputSystem.actions.FindAction("CameraControl");
     }
 
-    void Update()
-    {
+    void Update() {
 
-        if (cameraAction.WasPressedThisFrame())
-        {
-            holding = true;
-            holdingScreenPos = Input.mousePosition;
-        }
-        if (cameraAction.WasReleasedThisFrame())
-        {
-            holding = false;
-            holdingScreenPos = Vector3.zero;
-        }
-
-        /*if (!holding)
-        {*/
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         if(matchPlayerHeight) {
             Plane plane = new Plane(Vector3.up, player.transform.position);
             plane.Raycast(ray, out float distance);
-            pointerPos = ray.origin + ray.direction * distance;
+            pointerPos = ray.origin + ray.direction * distance + Vector3.up * yOffset;
         } else {
             if(Physics.Raycast(ray, out RaycastHit hitData, raycastDistance, layerMask)) {
-                pointerPos = hitData.point;
+                pointerPos = hitData.point + Vector3.up * yOffset;
             }
         }
-        /*}
-        else
-        {
-            Ray ray = cam.ScreenPointToRay(holdingScreenPos);
-
-            if (Physics.Raycast(ray, out RaycastHit hitData, raycastDistance, layerMask))
-            {
-                pointerPos = hitData.point;
-            }
-        }*/
-
 
 
         // LIMIT POINTERPOS TO UNIT CIRCLE RADIUS OF X FROM PLAYER
+        Vector3 toPointerVect = (pointerPos - player.transform.position);
+        pointerPos = player.transform.position + Vector3.ClampMagnitude(toPointerVect, maxRange);
 
 
-        if (pointerPos != null)
-        {
-            if(matchPlayerHeight)
-                pointerPos.y = player.transform.position.y + yOffset;
-            else
-                pointerPos.y += yOffset;
+        if(pointerPos != null) {
             transform.position = Vector3.MoveTowards(transform.position, pointerPos, Time.deltaTime * flySpeed);
         }
     }
