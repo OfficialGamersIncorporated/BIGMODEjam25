72d336e4a00bafa59ef9258032f9d067b6ba9d83
Chris <32988106+Noobot9k@users.noreply.github.com>
2025-01-31 03:01:20 -0700

    flat tires and flat tire shader
    tweak

---
+++ Assets/_Scripts/VehicleHealth.cs
@@ -0,0 +1,37 @@
+using UnityEditor;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class VehicleHealth : MonoBehaviour {
+
+    Vehicle vehicleControl;
+
+    void Start() {
+        vehicleControl = GetComponent<Vehicle>();
+    }
+    void Update() {
+
+    }
+
+    public bool TryPopATire() {
+        foreach(WheelControl wheel in vehicleControl.wheels) {
+            if(wheel.isFlat)continue;
+
+            wheel.isFlat = true;
+            return true;
+        }
+        return false;
+    }
+}
+
+[CustomEditor(typeof(VehicleHealth))]
+public class VehicleHealthEditor : Editor {
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        VehicleHealth myScript = (VehicleHealth)target;
+        if(GUILayout.Button("Pop a tire!")) {
+            myScript.TryPopATire();
+        }
+    }
+}
--- Assets/_Scripts/WheelControl.cs
+++ Assets/_Scripts/WheelControl.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class WheelControl : MonoBehaviour {
@@ -10,16 +11,46 @@ public class WheelControl : MonoBehaviour {
     // Editor Inspector window)
     public bool steerable;
     public bool motorized;
+    private bool _isFlat;
+    public bool isFlat {
+        get {
+            return _isFlat;
+        }
+        set {
+            _isFlat = value;
+
+            // these variables HAVE to be defined. If you try to set stiffness all in one line you'll get an error.
+            WheelFrictionCurve forwardCurve = WheelCollider.forwardFriction;
+            WheelFrictionCurve sidewaysCurve = WheelCollider.sidewaysFriction;
+
+            if(_isFlat) {
+                WheelCollider.radius = flatRadius;
+                forwardCurve.stiffness = flatStiffness;
+                sidewaysCurve.stiffness = flatStiffness;
+            } else {
+                WheelCollider.radius = startRadius;
+                forwardCurve.stiffness = 1;
+                sidewaysCurve.stiffness = 1;
+            }
+
+            WheelCollider.forwardFriction = forwardCurve;
+            WheelCollider.sidewaysFriction = sidewaysCurve;
+        }
+    }
+
+    public float flatRadius = 0.15f;
+    public float flatStiffness = 0.25f;
 
     Vector3 position;
     Quaternion rotation;
 
-    // Start is called before the first frame update
+    float startRadius;
+
     private void Start() {
         WheelCollider = GetComponent<WheelCollider>();
+        startRadius = WheelCollider.radius;
     }
 
-    // Update is called once per frame
     void Update() {
         // Get the Wheel collider's world pose values and
         // use them to set the wheel model's position and rotation
