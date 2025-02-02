2a27388911500acad7052b6cc336a9d0f9ec5ed0
SwPGameDev <swpgamedev@gmail.com>
2025-02-01 20:28:57 -0700

    Enemy on hit cooldown

--- Assets/_Scripts/DamageOnContact.cs
+++ Assets/_Scripts/DamageOnContact.cs
@@ -1,15 +1,62 @@
 using UnityEngine;
 
-public class DamageOnContact : MonoBehaviour {
+public class DamageOnContact : MonoBehaviour
+{
+    public float hitCooldown = 0.25f;
+    float hitTimer = 0;
+    bool canHit = true;
+    public float minVelocity = 1;
+    float linearVelocity;
 
     public bool DestroyOnDamage = false;
 
-    private void OnCollisionEnter(Collision collision) {
-        VehicleHealth damagable = collision.gameObject.GetComponent<VehicleHealth>();
-        if(!damagable) return;
+    Rigidbody rb;
 
-        damagable.TryDamage();
-        if (DestroyOnDamage) Destroy(gameObject);
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+
+        if (!canHit)
+        {
+            hitTimer += Time.deltaTime;
+
+            if (hitTimer >= hitCooldown)
+            {
+                canHit = true;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        linearVelocity = rb.linearVelocity.magnitude;
+        if (linearVelocity < minVelocity)
+        {
+            canHit = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!canHit)
+        {
+            print("ON COOLDOWN");
+            return;
+        }
+        else 
+        {
+            VehicleHealth damagable = collision.gameObject.GetComponent<VehicleHealth>();
+            if (!damagable) return;
+
+            damagable.TryDamage();
+            if (DestroyOnDamage) Destroy(gameObject);
+
+            canHit = false;
+            print("HIT: " + collision.gameObject.name);
+        }
+    }
 }
