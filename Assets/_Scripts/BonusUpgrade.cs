099cb46d13591167c282791bbeadd6450316571e
Chris <32988106+Noobot9k@users.noreply.github.com>
2025-02-02 06:22:56 -0700

    file system cleanup

--- Assets/_Scripts/BonusUpgrade.cs
+++ Assets/_Scripts/BonusUpgrade.cs
@@ -1,56 +1,45 @@
 using System;
 using UnityEngine;
 
-public class BonusUpgrade : MonoBehaviour
-{
+public class BonusUpgrade : MonoBehaviour {
     UpgradeManager upgradeManager;
-    [SerializeField] GameObject mcGuffin;
+    [SerializeField] Rigidbody mcGuffin;
     [SerializeField] float waitTime = 1;
     float tempWaitTimer;
 
     bool triggered = false;
 
     bool upgradeGiven = false;
-    
-    void Start()
-    {
-        if (mcGuffin == null)
-        {
+
+    void Start() {
+        if(mcGuffin == null) {
             Debug.LogError("NO MCGUFFIN WTF");
         }
         upgradeManager = UpgradeManager.Instance;
-        if (upgradeManager == null)
-        {
+        if(upgradeManager == null) {
             Debug.LogError("NO UPGRADE MANAGER OH NO");
         }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject == mcGuffin && !upgradeGiven)
-        {
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject == mcGuffin.gameObject && !upgradeGiven) {
             triggered = true;
             Debug.Log("MCGUFFIN IN");
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject == mcGuffin && !upgradeGiven)
-        {
+    private void OnTriggerExit(Collider other) {
+        if(other.gameObject == mcGuffin.gameObject && !upgradeGiven) {
             triggered = false;
             tempWaitTimer = 0;
             Debug.Log("MCGUFFIN REMOVED");
         }
     }
 
-    private void Update()
-    {
-        if (triggered && !upgradeGiven)
-        {
+    private void Update() {
+        if(triggered && !upgradeGiven && mcGuffin.IsSleeping()) {
             tempWaitTimer += Time.deltaTime;
-            if (tempWaitTimer >= waitTime)
-            {
+            if(tempWaitTimer >= waitTime) {
                 // GIVE UPGRADE WAHOO
                 Debug.Log("UPGRADE GET");
                 upgradeGiven = true;
