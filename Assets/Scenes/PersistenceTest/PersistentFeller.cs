f3bc589b9cd9c657c9812148cebc4c616ed8b016
SwPGameDev <swpgamedev@gmail.com>
2025-02-02 18:01:09 -0700

    Bonus objective tracking

--- Assets/Scenes/PersistenceTest/PersistentFeller.cs
+++ Assets/Scenes/PersistenceTest/PersistentFeller.cs
@@ -1,21 +1,37 @@
 using System;
 using TMPro;
 using UnityEngine;
+using static BonusUpgrade;
 
 public class PersistentFeller : MonoBehaviour
 {
     public static float timer;
 
-    public bool bonus1 = false;
-    public bool bonus2 = false;
-    public bool bonus3 = false;
-    public bool bonus4 = false;
-    public bool bonus5 = false;
+    public static int TiresCollected;
+
+    public static bool bonus1Achieved = false;
+    public static bool bonus2Achieved = false;
+    public static bool bonus3Achieved = false;
+    public static bool bonus4Achieved = false;
+    public static bool bonus5Achieved = false;
 
     public TextMeshProUGUI text;
 
+
+    private static PersistentFeller _instance;
+    public static PersistentFeller Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.LogError("PersistentFeller is null");
+            return _instance;
+        }
+    }
+
     private void Awake()
     {
+        _instance = this;
         text.text = ("0.00");
     }
 
@@ -23,5 +39,27 @@ public class PersistentFeller : MonoBehaviour
     {
         timer += Time.deltaTime;
         text.text = ("Time: ") + Math.Round(timer, 2, MidpointRounding.AwayFromZero);
+    }
+
+    public void StoreBonus(BonusUpgrade.BonusID IDParam)
+    {
+        switch (IDParam)
+        {
+            case BonusID.bonus1:
+                bonus1Achieved = true;
+                break;
+            case BonusID.bonus2:
+                bonus2Achieved = true;
+                break;
+            case BonusID.bonus3:
+                bonus3Achieved = true;
+                break;
+            case BonusID.bonus4:
+                bonus4Achieved = true;
+                break;
+            case BonusID.bonus5:
+                bonus5Achieved = true;
+                break;
+        }
     }
 }
--- Assets/_Scripts/BonusUpgrade.cs
+++ Assets/_Scripts/BonusUpgrade.cs
@@ -3,33 +3,39 @@ using UnityEngine;
 
 public class BonusUpgrade : MonoBehaviour {
     UpgradeManager upgradeManager;
+    PersistentFeller persistentFeller;
     [SerializeField] Rigidbody mcGuffin;
     [SerializeField] float waitTime = 1;
     float tempWaitTimer;
 
     bool triggered = false;
 
-    bool upgradeGiven = false;
+    bool bonusStored = false;
+
+    public enum BonusID { bonus1, bonus2, bonus3, bonus4, bonus5 }
+
+    public BonusID mybonusID;
 
     void Start() {
         if(mcGuffin == null) {
             Debug.LogError("NO MCGUFFIN WTF");
         }
         upgradeManager = UpgradeManager.Instance;
+        persistentFeller = PersistentFeller.Instance;
         if(upgradeManager == null) {
             Debug.LogError("NO UPGRADE MANAGER OH NO");
         }
     }
 
     private void OnTriggerEnter(Collider other) {
-        if(other.gameObject == mcGuffin.gameObject && !upgradeGiven) {
+        if(other.gameObject == mcGuffin.gameObject && !bonusStored) {
             triggered = true;
             Debug.Log("MCGUFFIN IN");
         }
     }
 
     private void OnTriggerExit(Collider other) {
-        if(other.gameObject == mcGuffin.gameObject && !upgradeGiven) {
+        if(other.gameObject == mcGuffin.gameObject && !bonusStored) {
             triggered = false;
             tempWaitTimer = 0;
             Debug.Log("MCGUFFIN REMOVED");
@@ -37,12 +43,13 @@ public class BonusUpgrade : MonoBehaviour {
     }
 
     private void Update() {
-        if(triggered && !upgradeGiven && mcGuffin.IsSleeping()) {
+        if(triggered && !bonusStored && mcGuffin.IsSleeping()) {
             tempWaitTimer += Time.deltaTime;
             if(tempWaitTimer >= waitTime) {
-                // GIVE UPGRADE WAHOO
-                Debug.Log("UPGRADE GET");
-                upgradeGiven = true;
+                
+                persistentFeller.StoreBonus(mybonusID);
+
+                bonusStored = true;
             }
         }
     }
